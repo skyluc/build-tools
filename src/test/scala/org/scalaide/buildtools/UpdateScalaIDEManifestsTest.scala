f648d9f1c25e38249d33664b6a328906f539d282
Luc Bourlier <luc.bourlier@typesafe.com>
2013-10-11 15:34:06 +0200

    Switch to Scala's bundles in UpdateAddonManifests
    And adds test

--- src/test/scala/org/scalaide/buildtools/UpdateScalaIDEManifestsTest.scala
+++ src/test/scala/org/scalaide/buildtools/UpdateScalaIDEManifestsTest.scala
@@ -7,15 +7,10 @@ import org.apache.commons.io.{ FileUtils => IOFileUtils }
 import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
-object TestData {
-
-  final val resourcesFolder = new File(System.getProperty("user.dir"), "src/test/resources")
-}
-
 class UpdateScalaIDEManifestsTest {
 
-  import TestData._
   import Ecosystem._
+  import TestSupport._
 
   @Test
   def releaseScalaVersion {
@@ -26,10 +21,10 @@ class UpdateScalaIDEManifestsTest {
     val sdtCoreManifest = new File(scalaIDERoot, "org.scala-ide.sdt.core/META-INF/MANIFEST.MF")
     val sdtDebugManifest = new File(scalaIDERoot, "org.scala-ide.sdt.debug/META-INF/MANIFEST.MF")
 
-    checkRequiredVersion(sdtCoreManifest, ScalaLangCompilerId, "2.10.2.v20130530-074427-VFINAL-60d462ef6e")
-    checkRequiredVersion(sdtCoreManifest, ScalaLangLibraryId, "2.10.2.v20130530-074427-VFINAL-60d462ef6e")
-    checkRequiredVersion(sdtDebugManifest, ScalaLangCompilerId, "2.10.2.v20130530-074427-VFINAL-60d462ef6e")
-    checkRequiredVersion(sdtDebugManifest, ScalaLangLibraryId, "2.10.2.v20130530-074427-VFINAL-60d462ef6e")
+    checkRequiredVersionInManifest(sdtCoreManifest, ScalaLangCompilerId, "2.10.2.v20130530-074427-VFINAL-60d462ef6e")
+    checkRequiredVersionInManifest(sdtCoreManifest, ScalaLangLibraryId, "2.10.2.v20130530-074427-VFINAL-60d462ef6e")
+    checkRequiredVersionInManifest(sdtDebugManifest, ScalaLangCompilerId, "2.10.2.v20130530-074427-VFINAL-60d462ef6e")
+    checkRequiredVersionInManifest(sdtDebugManifest, ScalaLangLibraryId, "2.10.2.v20130530-074427-VFINAL-60d462ef6e")
   }
 
   @Test
@@ -41,39 +36,11 @@ class UpdateScalaIDEManifestsTest {
     val sdtCoreManifest = new File(scalaIDERoot, "org.scala-ide.sdt.core/META-INF/MANIFEST.MF")
     val sdtDebugManifest = new File(scalaIDERoot, "org.scala-ide.sdt.debug/META-INF/MANIFEST.MF")
 
-    checkRequiredVersion(sdtCoreManifest, ScalaLangCompilerId, "2.10.3.v20130924-143159-9f62900145")
-    checkRequiredVersion(sdtCoreManifest, ScalaLangLibraryId, "2.10.3.v20130924-143159-9f62900145")
-    checkRequiredVersion(sdtDebugManifest, ScalaLangCompilerId, "2.10.3.v20130924-143159-9f62900145")
-    checkRequiredVersion(sdtDebugManifest, ScalaLangLibraryId, "2.10.3.v20130924-143159-9f62900145")
+    checkRequiredVersionInManifest(sdtCoreManifest, ScalaLangCompilerId, "2.10.3.v20130924-143159-9f62900145")
+    checkRequiredVersionInManifest(sdtCoreManifest, ScalaLangLibraryId, "2.10.3.v20130924-143159-9f62900145")
+    checkRequiredVersionInManifest(sdtDebugManifest, ScalaLangCompilerId, "2.10.3.v20130924-143159-9f62900145")
+    checkRequiredVersionInManifest(sdtDebugManifest, ScalaLangLibraryId, "2.10.3.v20130924-143159-9f62900145")
   }
 
-  private def checkRequiredVersion(file: File, bundleId: String, version: String) {
-    val s = Source.fromFile(file, "UTF-8")
-
-    val lines = s.getLines.filter(_.contains(bundleId)).toList
-
-    assertEquals("Too many lines with " + bundleId, 1, lines.size)
-    lines.foreach { l =>
-      assertEquals("Invalid required bundle line", s""" ${bundleId};bundle-version="[${version},${version}]",""", l)
-    }
-
-  }
-
-  private def createTestEnv(baseFolders: String*): List[File] = {
-    val tmpFolder = new File("target/tmp-test")
-
-    tmpFolder.mkdirs()
-    IOFileUtils.cleanDirectory(tmpFolder)
-
-    val result = ListBuffer[File]()
-
-    for (folder <- baseFolders) {
-      val newFolder = new File(tmpFolder, folder)
-      IOFileUtils.copyDirectory(new File(resourcesFolder, folder), newFolder)
-      result.append(newFolder)
-    }
-
-    result.toList
-  }
 
 }
