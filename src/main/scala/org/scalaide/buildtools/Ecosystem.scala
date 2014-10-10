f2c6e7b76211e77f364fc75630584ed0f26b66ef
Luc Bourlier <luc.bourlier@typesafe.com>
2014-10-10 16:11:29 +0200

    Fixes typo in luna repo URL

--- src/main/scala/org/scalaide/buildtools/Ecosystem.scala
+++ src/main/scala/org/scalaide/buildtools/Ecosystem.scala
@@ -130,7 +130,7 @@ object Ecosystem {
 
   case object EclipseKepler extends EclipseVersion("kepler", "Kepler", "http://download.eclipse.org/releases/kepler/")
 
-  case object EclipseLuna extends EclipseVersion("luna", "Luna", "http://download.eclipse.org/release/luna/")
+  case object EclipseLuna extends EclipseVersion("luna", "Luna", "http://download.eclipse.org/releases/luna/")
 
   def findStrictVersion(range: String): Version =
     findStrictVersion(VersionRange(range))
